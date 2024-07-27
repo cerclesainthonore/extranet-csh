d4ccbb0123c2de0167d81965d619bd0cb2531936
Noah Ravaux <ravaux.noah@gmail.com>
2024-07-27 19:29:42 +0200

    frontend: navbar select attempt

--- frontend-csh/src/components/dropdown/dropdown.tsx
+++ frontend-csh/src/components/dropdown/dropdown.tsx
@@ -14,15 +14,19 @@ const Dropdown = (props: IDropdownProps) => {
     return (
         <div
             className={"csh-dropdown " + props.className}
-            onClick={() => {setOpen(!open)}}
-            //onBlur={() => setOpen(false)}
+            onClick={() => {
+                setOpen(!open)
+            }}
+            onBlur={() => setOpen(false)}
             tabIndex={0}
         >
             <div className="csh-dropdown-title">
-                <span>{props.title}</span>
+                <span>
+                    {props.title}
+                    <div className={`csh-dropdown-arrow ${open ? "active" : ""}`}></div>
+                </span>
             </div>
-            <div className={`csh-dropdown-arrow ${open ? "active" : ""}`}></div>
-            <div className={`csh-dropdown-options ${open ? "active": ""}`}>
+            <div className={`csh-dropdown-options ${open ? "active" : ""}`}>
                 {props.options.map((option: ReactNode, index: number) => {
                     return (
                         <div className="csh-dropdown-option" key={index}>
