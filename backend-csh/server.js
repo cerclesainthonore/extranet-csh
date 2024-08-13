21c1f4ebc69e19e1a54bb8029bfbaf81a8276780
Noahrav <ravaux.noah@gmail.com>
2024-08-13 15:20:48 +0200

    backend: removed mongodb

--- backend-csh/server.js
+++ backend-csh/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const {log, error} = require("./utils/logging");
@@ -8,28 +7,12 @@ const nodemailer = require("nodemailer");
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
-const dbHost = process.env.MONGODB_HOST;
-const dbUsername = process.env.MONGODB_USERNAME;
-const dbPassword = process.env.MONGODB_PASSWORD;
-const mongoURI = `mongodb://${dbUsername}:${dbPassword}@${dbHost}:27017/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`;
+const port = "3000";
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-
-
-mongoose.connect(mongoURI);
-
-const db = mongoose.connection;
-db.on("error", (err) => {
-    console.error.bind(console, "connection error:");
-    error("MongoDB connection error: " + err);
-});
-db.once("open", () => {
-    log("Connected to MongoDB");
-});
 
 const feedbackEmail = 'noreply@cerclesainthonore.fr';
 
