24c584974f0a1653e24acad5e66f7d7ea1a621f9
saintroch <166071604+cerclesainthonore@users.noreply.github.com>
2024-11-14 10:49:53 +0100

    Update backend-csh/config/index.js

--- backend-csh/config/index.js
+++ backend-csh/config/index.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 const Config = {
     port: process.env.PORT ?? "3000",
-    mongoUser: process.env.MONGODB_USER ?? "",
+    mongoUser: process.env.MONGODB_USER ?? process.env.NODE_ENV === "production" ? "" : "admin:admin",
     mongoHost: process.env.MONGODB_HOST ?? "localhost",
     mongoCollection: process.env.MONGODB_COLLECTION ?? "CSH",
     origin: process.env.ORIGIN ?? "*"
